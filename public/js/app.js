1646146adc4388a88204ab0664c8f7facad3e7f5
Zorro <badass.commits@gmail.com>
2020-05-10 15:14:49 -0400

    seeds and queues

--- public/js/app.js
+++ public/js/app.js
@@ -37412,8 +37412,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Axel\Documents\code\YouTube\laranotification\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Axel\Documents\code\YouTube\laranotification\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__("./resources/js/app.js");
+module.exports = __webpack_require__("./resources/sass/app.scss");
 
 
 /***/ })
